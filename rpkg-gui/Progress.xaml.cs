7276a3bae50181992097eb22b9a66ec163066751
Reece Dunham <me@rdil.rocks>
2023-04-19 17:39:14 +0200

    "it works"

--- rpkg-gui/Progress.xaml.cs
+++ rpkg-gui/Progress.xaml.cs
@@ -1,21 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using System.Threading;
 using System.ComponentModel;
 using System.Runtime.InteropServices;
 using MahApps.Metro.Controls;
-using ControlzEx.Theming;
 
 namespace rpkg
 {
@@ -39,6 +27,7 @@ namespace rpkg
 
             worker.RunWorkerAsync();
         }
+
         protected override void OnRenderSizeChanged(SizeChangedInfo sizeInfo)
         {
             base.OnRenderSizeChanged(sizeInfo);
@@ -306,7 +295,7 @@ namespace rpkg
 
             if (operation != (int)Operation.IMPORT)
             {
-                message.Content = timing_string;
+                message.Content = "Performing requested actions (this may take a moment!)";
             }
         }
 
